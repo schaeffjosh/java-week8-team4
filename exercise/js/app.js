f0c5006a35bb9b40ce4922b58b5265373e80832f
Josh Schaeffer <schaeffjosh@gmail.com>
2023-11-17 14:57:19 -0500

    Get initial functionality

--- exercise/js/app.js
+++ exercise/js/app.js
@@ -1,30 +1,60 @@
 let answer;
-let userAnswer;
+let probNum = 1;
+let score = 0;
 function getRandomNumber(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-function buildDisplayProblem(num1, num2, operator) {
+function buildProblem(opArr) {
+    let num1 = getRandomNumber(9);
+    let num2 = getRandomNumber(9);
+    opArr = shuffleArray(opArr);
+    let selectedOperator = opArr[0];
+    displayProblem(num1, num2, selectedOperator);
+    if(selectedOperator === "+") {
+        answer = num1 + num2;
+        buildDisplayAnswers(num1 + num2);
+    } else if (selectedOperator === "-") {
+        answer = num1 - num2;
+        buildDisplayAnswers(num1 - num2);
+    } else if (selectedOperator === "*") {
+        answer = num1 * num2;
+        buildDisplayAnswers(num1 * num2);
+    } else if (selectedOperator === "/") {
+        answer = num1 / num2;
+        buildDisplayAnswers(num1/num2);
+    }
+}
+
+function displayProblem(num1, num2, operator) {
     let probString = num1 + operator + num2;
     const probBar = document.querySelector(".expression");
     probBar.innerText = probString;
 }
 
 function buildDisplayAnswers(answer) {
-    let ansArr = [answer, getRandomNumber(81), getRandomNumber(81), getRandomNumber(81)];
+    let ansArr = [answer, getRandomNumber(81) - getRandomNumber(81), getRandomNumber(81) - getRandomNumber(81), getRandomNumber(81) - getRandomNumber(81)];
     ansArr = shuffleArray(ansArr);
 
     const ansItems = document.querySelectorAll("li");
     for(let i = 0; i < ansItems.length; i++){
         ansItems[i].innerText = ansArr[i];
         ansItems[i].addEventListener("click", (event) => {
-            selectAns(event.target);
+            selectAns(event.target.value);
         })
     }
 }
 
-function selectAns(answer) {
-    
+function selectAns(userAnswer) {
+    let currentProblem = document.querySelector(".currentProblem");
+    probNum++;
+    currentProblem.innerText = probNum;
+
+    if(userAnswer === answer){
+        let scoreBox = document.querySelector(".currentScore");
+        score++;
+        scoreBox.innerText = score;
+    }
 }
 
 /**
@@ -32,11 +62,9 @@ function selectAns(answer) {
 * @param {object} arr
 */
 function shuffleArray(arr) {
-    return arr.sort(function (a, b) { return Math.random() - 0.5 })
+    return arr.sort(function (a, b) { return Math.random() - 0.5 });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    buildDisplayProblem(4, 9, "+");
-
-    buildDisplayAnswers(13);
+    buildProblem(["+", "-", "*", "/"]);
 })
